b2a04dab7283cf5c3860122184a5111ace435fdb
Meigan <Meigan@Meigans-MacBook-Pro.local>
2024-09-14 14:41:47 -0700

    major update (new website)

---
+++ app.js
@@ -0,0 +1,121 @@
+let opener = document.querySelector('.opener');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo');
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+
+        setTimeout(() => {
+            logoSpan.forEach((span, idx) => {
+                setTimeout(() => {
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50)
+            });
+        }, 2000);
+
+        setTimeout(() => {
+            opener.style.top = '-100vh';
+        }, 2300);
+    });
+})
+
+
+const moveElements = (e) => {
+    const shapes = document.querySelectorAll(".shape");
+    const tracker = document.querySelector(".tracker");
+
+    tracker.style.top = `${e.clientY}px`;
+    tracker.style.left = `${e.clientX}px`;
+    tracker.style.opacity = 1;
+
+    shapes.forEach((shape) => {
+        const shapeOffset = shape.getAttribute("data-offset");
+
+        let offsetX = (window.innerWidth - e.clientX) * shapeOffset;
+        let offsetY = (window.innerHeight - e.clientY) * shapeOffset;
+
+        shape.style.translate = `${offsetX}px ${offsetY}px`;
+    });
+};
+
+document.addEventListener("mousemove", moveElements);
+
+let listTab = document.querySelectorAll('.tab');
+let titleBanner = document.querySelector('.banner h1');
+window.addEventListener("scroll", (event) => {
+    /*scrollY is the web scrollbar position (pixel)*/
+    let top = window.scrollY;
+    /* parallax scroll in tab
+    when tab's position is less than 550 px
+    from scrollbar position add active class to animate 
+    and vice versa
+    */
+    listTab.forEach(tab =>{
+        if(tab.offsetTop - top < 550){
+            tab.classList.add('active');
+        }else{
+            tab.classList.remove('active');
+        }
+    })
+});  
+
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let carousel = document.querySelector('.carousel');
+let items = document.querySelectorAll('.carousel .item');
+let countItem = items.length;
+let active = 1;
+let other_1 = null;
+let other_2 = null;
+next.onclick = () => {
+    carousel.classList.remove('prev');
+    carousel.classList.add('next');
+    active =active + 1 >= countItem ? 0 : active + 1;
+    other_1 =active - 1 < 0 ? countItem -1 : active - 1;
+    other_2 = active + 1 >= countItem ? 0 : active + 1;
+    changeSlider();
+}
+prev.onclick = () => {
+    carousel.classList.remove('next');
+    carousel.classList.add('prev');
+    active = active - 1 < 0 ? countItem - 1 : active - 1;
+    other_1 = active + 1 >= countItem ? 0 : active + 1;
+    other_2 = other_1 + 1 >= countItem ? 0 : other_1 + 1;
+    changeSlider();
+}
+const changeSlider = () => {
+    let itemOldActive = document.querySelector('.carousel .item.active');
+    if(itemOldActive) itemOldActive.classList.remove('active');
+
+    let itemOldOther_1 = document.querySelector('.carousel .item.other_1');
+    if(itemOldOther_1) itemOldOther_1.classList.remove('other_1');
+
+    let itemOldOther_2 = document.querySelector('.carousel .item.other_2');
+    if(itemOldOther_2) itemOldOther_2.classList.remove('other_2');
+
+    items.forEach(e => {
+        e.querySelector('.image img').style.animation = 'none';
+        e.querySelector('.image figcaption').style.animation = 'none';
+        void e.offsetWidth;
+        e.querySelector('.image img').style.animation = '';
+        e.querySelector('.image figcaption').style.animation = '';
+    })
+
+    items[active].classList.add('active');
+    items[other_1].classList.add('other_1');
+    items[other_2].classList.add('other_2');
+
+    clearInterval(autoPlay);
+    autoPlay = setInterval(() => {
+        next.click();
+    }, 5000);
+}
+let autoPlay = setInterval(() => {
+    next.click();
+}, 5000);
